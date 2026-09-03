e6573b25f70bbfeac6ab57152f335707236a1df2
enesimrakresmi <enesimrakresmi@gmail.com>
2026-09-03 17:52:49 +0300

    fix: adjust mobile top safe-area padding for notch/status bar

--- components/ClientShell.js
+++ components/ClientShell.js
@@ -15,7 +15,7 @@ export default function ClientShell({ children }) {
         <PushAutoSubscribe />
         <NotificationModal />
         <Navigation />
-        <main className="min-h-screen px-2 pb-24 pt-3 sm:px-6 sm:pb-28 sm:pt-6 md:ml-20 md:px-10 md:pb-8 md:pt-6">
+        <main className="min-h-screen px-2.5 pb-24 pt-[calc(1.25rem+env(safe-area-inset-top,0px))] sm:px-6 sm:pb-28 sm:pt-6 md:ml-20 md:px-10 md:pb-8 md:pt-6">
           {children}
         </main>
       </NotificationProvider>
